88305ed24bf61d07ea9a31b2f2f78e654e80e1f7
YEIM <yeim@YEIMui-MacBookAir.local>
2021-04-12 22:57:52 +0900

    전체 커밋

--- matzip-web/src/index.js
+++ matzip-web/src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './App.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
